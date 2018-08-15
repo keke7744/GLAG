b8fe1e939eace3f1073bc48252e624f5de77b77a
keke7744 <41632496+keke7744@users.noreply.github.com>
2018-08-15 14:04:49 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,7 @@ window.onload = function() {
     'Hey there 👋',
     'I\'m Achraf filali',
     'I design and code things on the web',
-    'I\'m currently accepting freelance work.<br> You can contact me at <a href="https://plus.google.com/u/0/115502341730046549286">ena yeser @mahleni.com</a>',
+    'I\'m currently accepting freelance work.<br> You can contact me at: <a>filali024@gmail.com </a>',
     '<a target="_blank" href="https://www.facebook.com/achraf.filali.904">facebook.com/achraf filali</a><br></a><br><a target="_blank" href="https://github.com/keke7744">github.com/achraf filali</a>',
     getCurrentTime(),
     '👀 AF.'
